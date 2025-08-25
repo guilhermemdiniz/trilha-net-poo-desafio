b992a3381ca8cd6926f9e4c3b0a58814796425fd
Guilherme <guidiniz2019.18@gmail.com>
2025-08-25 00:33:18 -0300

    desafio concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,23 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
 
-        public Smartphone(string numero)
+        public string Numero { get; set; }
+        public string Modelo { get => _modelo; }
+        public string IMEI { get => _imei; }
+        public int Memoria { get => _memoria; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Intanciando objeto da classe Smartphone (não deve permitir, pois é uma classe abstrata)
+// Smartphone s = new Smartphone(numero: "989824323", modelo: "J7 Prime", imei: "23923834234", memoria: 8);
+
+// Instanciando objeto da classe Iphone
+Iphone i = new Iphone(numero: "983747434", modelo: "6s", imei: "873546734353", memoria: 3);
+// Executando método InstalarAplicativo no Iphone
+i.InstalarAplicativo(nomeApp: "Instagram");
+
+// Instanciando objeto da classe Nokia
+Nokia n = new Nokia(numero: "987645533", modelo: "Tijolão", imei: "32875832752", memoria: 1);
+n.InstalarAplicativo("Jogo da cobrinha");
